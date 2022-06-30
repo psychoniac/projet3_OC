7ad1a931f1ae6cf407a5fb1b0f3ccabf172964f4
jlnko <105540318+psychoniac@users.noreply.github.com>
2022-06-30 02:36:53 +0200

    Update connexion_bdd.php

--- connexion_bdd.php
+++ connexion_bdd.php
@@ -1,11 +1,10 @@
 <?php
 try
-{
-	$db = new PDO('mysql:host=localhost;dbname=projet3;charset=utf8', 'root', '');
-	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (Exception $e)
-{
-        die('Erreur : ' . $e->getMessage());
+ {
+    $db = new PDO( 'mysql:host=localhost;dbname=projet3;charset=utf8', 'root', '' );
+    $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+} catch ( Exception $e )
+ {
+    die( 'Erreur : ' . $e->getMessage() );
 }
 ?>
