78ea5ef3e3fa9002dae92af859e97f29b17b1025
jlnko <105540318+psychoniac@users.noreply.github.com>
2022-07-03 16:31:20 +0200

    Update acteur.php
    mis a jour du code

--- acteur.php
+++ acteur.php
@@ -20,56 +20,37 @@
 ?>
 
 <h2>Les partenaires</h2>
-<?php if ( isset( $_GET[ 'id' ] ) ) : ?>
-
-<?php
+<?php if (isset($_GET[ 'id' ])) {
     $requeteSQL = 'SELECT * FROM acteur WHERE id_acteur = :id';
     $requete = $db->prepare( $requeteSQL );
     $acteurselect = $requete->execute( [ 'id' => $_GET[ 'id' ] ] );
     $acteur = $requete->fetch();
-?>
-<?php if (!empty( $acteur ) ) : ?>
+        if (!empty($acteur)) { ?>
 
         <img class = 'logo_acteur' src = "img/<?php echo $acteur['logo']; ?>"></br>
+
         <div class = 'nom_acteur2'>
-            <?php	echo $acteur[ 'acteur' ];?></br>
+        <?php	echo $acteur[ 'acteur' ];?></br>
         </div>
+
         <div class = 'description'>
-            <?php echo $acteur[ 'description' ]; ?>
+        <?php echo $acteur[ 'description' ]; ?>
         </div>
+        
         <a href = 'like_dislike.php?vote=1&acteurId=<?php echo $_GET['id']; ?>&user=<?php echo $_SESSION['username']; ?>' >Like</a>
         <a href = 'like_dislike.php?vote=0&acteurId=<?php echo $_GET['id']; ?>&user=<?php echo $_SESSION['username']; ?>' >Dislike</a>
-        <form method = 'post' action = 'acteur.php'>
-        <textarea name = 'commentaire' name = 'commentaire' rows = '5' cols = '250'></textarea>
-        <input type = 'submit' name = 'envoyer'>
-        </form>
-        </section>
-
-<?php endif; ?>
-<?php endif; ?>
-
-
-
-<?php echo date( 'l \l\e jS' );
+        
+<?php //parti commentaire
 echo $_SESSION[ 'username' ];
-?>
-</br>
-<?php echo 'peut poster un commentaire';
-?>
+echo 'peut poster un commentaire'; ?>
 
 <form method = 'post' action = 'acteur.php'>
 <textarea name = 'commentaire' name = 'commentaire' rows = '5' cols = '250'></textarea>
 <input type = 'submit' name = 'envoyer'>
 </form>
-</section>
 
-<?php //on verifie que l'utilisateur est bien connecté via $_SESSION['id'],et que $_post envoyer et commentaire sont bien défini
-		//if (isset($_SESSION['username']) && !empty($_POST['envoyer']) && !empty('commentaire')) {
-		 //	$commentaire = $_POST['commentaire' ];
-//	$requete = prepare()
-//}
 
-?>
+<?php } } ?>
 
 </body>
 
