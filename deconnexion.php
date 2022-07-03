e6bdee75ffe9e818a29c770b58b32f25c087850c
jlnko <105540318+psychoniac@users.noreply.github.com>
2022-07-03 13:35:15 +0200

    Update deconnexion.php
    redirection vers login.php si $_session n'existe pas

--- deconnexion.php
+++ deconnexion.php
@@ -1,5 +1,10 @@
-<?php session_start() ; 
-    if (isset($_POST['deconnexion'])) {
+<?php if (!isset($_SESSION['id'])) {
+    header('Location: login.php'); }
+    else {
+        session_start();
+    }?>
+
+<?php    if (isset($_POST['deconnexion'])) {
         session_destroy();
     }
 ?>
