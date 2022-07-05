82f1de072b338474466358c9741b5a06d58500cd
jlnko <105540318+psychoniac@users.noreply.github.com>
2022-07-05 09:57:11 +0200

    Update login.php
    modif html

--- login.php
+++ login.php
@@ -43,20 +43,53 @@ if (isset($_POST['envoyer'])) {
 <head>
 		<meta charset="utf-8" />
 		<link rel="stylesheet" href="style.css">
-		<title>login</title>
+		<title>Projet3</title>
 </head>
 
 <body>
-<?php include('en_tete.php'); ?>
-	<h1>Se connecter :</h1>
-	<form class='login' method="post" action="login.php">
+<div class="container">
+<div class="en_tete">
+<img class="logo_gbaf" src="img/logo.png" alt="logo couleur de la société" width="150px" height="150px"  title="logo de l'entreprise">
+</div>
+<div class="corps_page">
+<h1>Se connecter :</h1>
+</br>
+</br>
+<form class='login' method="post" action="login.php">
 		<label for="identifiant">Mon identifiant</label>
 		<input type="text" name="identifiant" label="identifiant" id="identifiant">
 		<label for="password">Mot de passe</label>
 		<input type="password" name="password" id="password">
 		<input type="submit" name="envoyer" label="Envoyer">
-	</form>
-	<a href="question_secrete.php">Mot de passe oublié...</a>
-<?php include('footer.php'); ?>
+</form>
+</br>
+</br>
+<a href="question_secrete.php">Mot de passe oublié...</a>
+</br>
+</div>
+	
+<div class="footer">
+	<div class=contact>
+<h3>Contact</h3>
+<ul>
+<li><p>Adresse: .......</p></li>
+<li><p>Téléphone:......</p></li>
+<li><p>E-mail:.........</p></li>
+</ul>
+</div>
+<div class=nos_reseaux>
+<h3>Nos réseaux</h3>
+<ul>
+<li><a href="https://twitter.com/share">Tweeter</a></li>
+<li><a href="mailto:jlnko@hotmail.fr">Contactez-moi</a></li>
+<li><a href="https://facebook.com/share">Facebook</a></li>
+</ul>
+</div>
+<div class="copyright">Copyright: GBAF Année 2022</div>
+</div>
+
+</div>
+</div>
+
 </body>
 </html>
