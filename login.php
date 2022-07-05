be238894a6fbd694796aa45787206d5ac4134001
jlnko <105540318+psychoniac@users.noreply.github.com>
2022-07-05 10:31:06 +0200

    Update login.php
    mis à jour code php pour verifier le mot de passe et ca fonctionne

--- login.php
+++ login.php
@@ -9,8 +9,7 @@ if (isset($_POST['envoyer'])) {
 	//on fait en sorte que les champs ne puisse pas contenir de code html
 	$username = htmlspecialchars($_POST['identifiant']);
 	$password = htmlspecialchars($_POST['password']);
-	//$passwordhach = password_hash($password, PASSWORD_DEFAULT);
-	//var_dump($password);
+	
 	//$passwordTest = password_verify($passwordhach,$usernameExist['password']);
 	//on va faire une requete pour savoir si le username est présent dans la base
 	$requete = $db->prepare('SELECT * FROM account WHERE username = ?');
@@ -19,14 +18,15 @@ if (isset($_POST['envoyer'])) {
 	//var_dump($usernameExist['password']);
 		//si le username existe 
 		if ($usernameExist !== false) {
+			$passwordBdd = $usernameExist['password'];
 			//on verifie le password
-			if ($usernameExist['password'] == $password) { 
+			if (password_verify($password,$passwordBdd)) {
 				$_SESSION['nom'] = $usernameExist['nom'];
 				$_SESSION['prenom'] = $usernameExist['prenom'];
 				$_SESSION['id'] = $usernameExist['id_user'];
 				header("Location: home.php");
 			} else {
-				echo "le password n'est pas bon";
+				echo "le mot de passe n'est pas le bon";
 			}
 		} else {
 			echo "le username n'est pas bon";
