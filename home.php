e79465f85410e7df65dd548f35ec5db4a297d3fd
jlnko <105540318+psychoniac@users.noreply.github.com>
2022-07-05 11:19:53 +0200

    Update home.php
    suppression d'une variable $_session id

--- home.php
+++ home.php
@@ -3,7 +3,7 @@ include( 'connexion_bdd.php' );
 if (!empty($_SESSION)) {
     echo $_SESSION['nom'];
     echo $_SESSION['prenom']; 
-    echo $_SESSION['id'];
+    
     echo "vous êtes bien connecté.";   
 } else {
     header('Location: login.php');
