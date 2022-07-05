5d84bc0e04c4f266dbdbbe0a99ac46ad86f922db
jlnko <105540318+psychoniac@users.noreply.github.com>
2022-07-05 14:40:31 +0200

    Update deconnexion.php
    mis a jour code

--- deconnexion.php
+++ deconnexion.php
@@ -1,26 +1,28 @@
 <?php session_start();
- if (isset($_SESSION['username'])) {
-    echo $_SESSION['username'];}
-else {
-    header(Location: "login.php");
-}
-?>
-
-<?php    if (isset($_POST['deconnexion'])) {
+include('connexion_bdd.php');
+ if (!empty($_SESSION)) {
+    echo $_SESSION['nom'];
+    echo $_SESSION['prenom'];
+    echo "vous êtes bien connecté.";
+    if (isset($_POST['deconnexion'])) {
         session_destroy();
-    }
-?>
+        echo "vous n'êtes plus conecté";
+    } 
+} else {
+    header(Location: "login.php");
+}?>
+
 
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset='utf-8'>
-    <title>projet3</title>
-    <link rel='stylesheet' type='text/css' media='screen' href='style.css'>
+    <title>Projet3</title>
+    <link rel='stylesheet' href='style.css'>
 </head>
 <body>
-   
-    <button name="deconnexion">Deconnexion</button>
-    
+<form>
+<input type="submit" name="deconnexion">Deconnexion</button>
+</form>
 </body>
 </html>
